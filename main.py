c26661f8226be5e8dd851fc1ce83c7f0648526c1
bee6523 <bee6523@kaist.ac.kr>
2020-11-23 20:37:25 +0900

    passing image to controlled_inpaint func

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import argparse
 import base64
+import numpy as np
+import cv2
 
 from flask import Flask, render_template, request
 from engine import controlled_inpaint
@@ -33,11 +35,25 @@ def showAttendResult():
     img_file=request.form['img']
     mask_file=request.form['mask']
     att_file=request.form['att']
-    print(base64.b64decode(mask_file))
-    print("img_loaded")
-    output = controlled_inpaint(image_path=img_file,
-                            mask_path=mask_file,
-                            att_path=att_file,
+
+    image_arr=np.frombuffer(base64.b64decode(img_file.split(',')[1]),dtype=np.uint8)
+    image=cv2.imdecode(image_arr,flags=cv2.IMREAD_COLOR)
+    mask_arr=np.frombuffer(base64.b64decode(mask_file.split(',')[1]),dtype=np.uint8)
+    mask=cv2.imdecode(mask_arr,flags=cv2.IMREAD_COLOR)
+    att_arr=np.frombuffer(base64.b64decode(att_file.split(',')[1]),dtype=np.uint8)
+    att=cv2.imdecode(att_arr,flags=cv2.IMREAD_COLOR)
+
+    img_path="./engine/input_image.png"
+    mask_path="./engine/input_mask.png"
+    att_path="./engine/input_att.png"
+    cv2.imwrite(img_path,image)
+    cv2.imwrite(mask_path,mask)
+    cv2.imwrite(att_path,att)
+
+    
+    output = controlled_inpaint(image_path=img_path,
+                            mask_path=mask_path,
+                            att_path=att_path,
                             out_image_path='./output_controlled.png')
 
     return render_template("pages/Upload.html")
